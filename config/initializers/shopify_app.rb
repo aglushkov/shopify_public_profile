fbab9f8c454265ac78d0cf33241982c6198ff981
Andrey Glushkov <aglushkov@shakuro.com>
2016-12-23 16:58:40 +0300

    Add remote script tag

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -3,4 +3,12 @@ ShopifyApp.configure do |config|
   config.secret = ENV['SHOPIFY_API_SECRET']
   config.scope = 'read_orders, read_products'
   config.embedded_app = true
+
+  config.scripttags = [
+    {
+      event: 'onload', src: (lambda do |_domain|
+        ActionController::Base.helpers.asset_url('shopify/test.js')
+      end)
+    }
+  ]
 end
