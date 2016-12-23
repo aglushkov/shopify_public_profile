a242eb88182d690374b166c786b2424182bde533
Andrey Glushkov <aglushkov@shakuro.com>
2016-12-23 17:17:16 +0300

    Fix remote script tag

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,14 +1,21 @@
 ShopifyApp.configure do |config|
   config.api_key = ENV['SHOPIFY_API_KEY']
   config.secret = ENV['SHOPIFY_API_SECRET']
-  config.scope = 'read_orders, read_products'
+  config.scope = [
+    :read_customers, :write_customers,
+    :read_products, :write_products,
+    :read_script_tags, :write_script_tags
+  ].join(', ')
   config.embedded_app = true
+end
 
-  config.scripttags = [
-    {
-      event: 'onload', src: (lambda do |_domain|
-        ActionController::Base.helpers.asset_url('shopify/test.js')
-      end)
-    }
-  ]
+ActiveSupport.on_load(:after_initialize) do
+  ShopifyApp.configure do |config|
+    config.scripttags = [
+      {
+        event: 'onload', display_scope: 'online_store',
+        src: ActionController::Base.helpers.asset_url('shopify/test.js')
+      }
+    ]
+  end
 end
